9105acf7bca19dff66b765cbed2c4290a11b27a6
matthewepler <matthewepler@gmail.com>
2016-02-10 16:46:14 +0000

    cleanup

--- wifi.js
+++ wifi.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
 var exec = require('child_process').exec;
+var async = require('async');
 
-function buildContents( SSID, psk, callback) {
+function buildContents( SSID, psk) {
 	var data = [];
 	 data[0] = [
 		"ctrl_interface=/var/run/wpa_supplicant",
@@ -25,27 +26,71 @@ function buildContents( SSID, psk, callback) {
 		"}"
 	].join("\n");
 
-	var dataString = data.join("\n\n");
-
-	fs.writeFile('/etc/wpa_supplicant/wpa_supplicant.conf', dataString, 'utf8', function() {
-			console.log('wpa_supplicant file written:');
-			console.log('SSID = ' + SSID);
-			console.log('psk = ' + psk);	
-			callback();
-	});
+	return data.join("\n\n");
 }
 
 function configure( SSID, psk ) {
-	var files = fs.readdirSync('/etc/wpa_supplicant/');
-	if (files.indexOf('wpa_supplicant.conf') > 0) {
-		exec('rm /etc/wpa_supplicant/wpa_supplicant.conf', function() { 
-			console.log('rm wpa_supplicant.conf');
-			buildContents( SSID, psk, function() {
-					console.log('WiFi config complete, restarting wlan');
-				});
-		});
-	}
+	async.waterfall([
+		function(cb) {
+			console.log('reading wpa_supplicant directory...');
+			fs.readdir('/etc/wpa_supplicant/', cb);
+		}, 
+		function(files, cb) {
+			console.log('removing configuration file...');
+			if (files.indexOf('wpa_supplicant.conf') > -1) {
+				exec('rm /etc/wpa_supplicant/wpa_supplicant.conf', cb);
+			} else {
+				cb(null);
+			}
+		}, 
+		function(cb) {
+			console.log('wpa_supplicant.conf deleted');
+			var dataString = buildContents(SSID, psk);
+			fs.writeFile('/etc/wpa_supplicant/wpa_supplicant.conf', dataString, 'utf8', cb(null));
+		},
+		function(cb) {
+			console.log('wlan0 going up');
+			exec('ifconfig wlan0 up', cb);
+		},
+		function(err, stdout, cb) {
+			if (err) console.log('err @ wlan0 up: ' + err);
+			if (stdout) console.log('stdout @ wlan0 up: ' + stdout);
+			console.log('killing wpa processes');
+			exec('killall wpa_supplicant', function(err, stdout, stderr) {
+				if (err) {
+					console.log(err);
+					cb(null);
+				} else if (stdout) {
+					console.log(stdout);
+					cb(null);
+				} else {
+					cb(null);
+				}	
+			});	
+		},
+		function(cb) {
+			console.log('restarting wpa...');
+			exec('wpa_supplicant -i wlan0 -D nl80211 -c /etc/wpa_supplicant/wpa_supplicant.conf -B', cb);
+		},
+		function(err, stdout, cb) {
+			if (err) console.log(err);
+			if (stdout) console.log(stdout);	
+			console.log('obtaining dhcp lease...');
+			exec('udhcpc -i wlan0', cb);
+		},
+		function(err, stdout, cb) {
+			if (err) console.log(err);
+			if (stdout) console.log(stdout);
+			cb();
+		}
+	], 
+	function(err, results){
+		if (err) console.log("ERROR: " + err);
+		// return something here
+	});
 }
 
-exports.config = configure;
+//configure("Human Condition Global 5GHz", "2900fl0z");
 //buildContents();
+exports.config = configure;
+
